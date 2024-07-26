24c969788ceb04441af96ad15ca05ec995a72481
TylerTheB <168641857+TylerTheB@users.noreply.github.com>
2024-07-26 14:45:47 +0500

    invite.py

--- plugins/invite.py
+++ plugins/invite.py
@@ -5,5 +5,5 @@ from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceRep
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
        		[ [ InlineKeyboardButton("📡 Sʜᴀʀᴇ Yᴏᴜʀ Lɪɴᴋ" ,url=f"https://t.me/share/url?url=https://t.me/{Config.BOT_USERNAME}?start={message.from_user.id}") ]   ])
-    await message.reply_text(f"**INVITE YOUR FRIENDS**",reply_markup=reply_markup,)
+    await message.reply_text(f"***BETA TESTING*",reply_markup=reply_markup,)
 
